97e5d4871593e9b4e4e77c93fd5428e59dfa38cf
cgfanning <cgottfanning@hotmail.com>
2020-07-31 13:15:46 -0600

    Express Sessions part 1

--- models/rsvp.js
+++ models/rsvp.js
@@ -2,7 +2,6 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 require('mongoose-currency').loadType(mongoose);
-const Currency = mongoose.Types.Currency;
 
 const rsvpSchema = new Schema({
     firstname: {
